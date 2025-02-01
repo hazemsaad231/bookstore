c0fb7327ed991b3598d12497a0f67485134b2840
hazemsaad231 <166450789+hazemsaad231@users.noreply.github.com>
2025-02-01 09:57:52 +0200

    ry

--- src/components/homeComponents/home.tsx
+++ src/components/homeComponents/home.tsx
@@ -3,9 +3,9 @@ import Slide from "./part1"
 import Category from "./part2"
 import New from "./part3"
 import Feature from "./part4"
-import Offer from "./part5"
-import Subscribe from "./part6"
-import Latest from "./part7"
+// import Offer from "./part5"
+// import Subscribe from "./part6"
+// import Latest from "./part7"
 
 
 const home = () => {
