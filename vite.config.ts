a3024efa416b56f482c76eb5e9db4e49ea3cb28e
hazemsaad231 <166450789+hazemsaad231@users.noreply.github.com>
2025-02-04 10:30:56 +0200

    npm

--- vite.config.ts
+++ vite.config.ts
@@ -9,9 +9,10 @@
 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import compression from "vite-plugin-compression";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), compression()],
   build: {
     minify: 'esbuild', // استخدام minifier سريع
     chunkSizeWarningLimit: 1000, // تجنب تحذيرات الملفات الضخمة
