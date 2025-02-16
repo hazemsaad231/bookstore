9c31d4ca84cb772541501f10a524a9197e09e9c0
hazemsaad231 <166450789+hazemsaad231@users.noreply.github.com>
2025-02-16 19:08:02 +0200

    ggg

--- src/components/login/image.tsx
+++ src/components/login/image.tsx
@@ -1,14 +1,25 @@
-import React from 'react';
-import img from '../../assets/img/img.jpeg'
+import { useState, useEffect } from "react";
+import img from "../../assets/img/img.jpeg";
+
 const Image = () => {
+    const [isVisible, setIsVisible] = useState(false);
     
+    useEffect(() => {
+        const observer = new IntersectionObserver(([entry]) => {
+            if (entry.isIntersecting) setIsVisible(true);
+        });
+        observer.observe(document.getElementById("lazy-image")!);
+        return () => observer.disconnect();
+    }, []);
 
-    return(
-        <>
-        
-            <img src={img} alt="" className=' hidden sm:hidden md:block lg:block xl:block h-screen w-1/2' />
-            </>
-    )
-}
+    return (
+        <img
+            id="lazy-image"
+            src={isVisible ? img : ""}
+            alt=""
+            className="hidden sm:hidden md:block lg:block xl:block h-screen w-1/2"
+        />
+    );
+};
 
-export default React.memo(Image);
+export default Image;
