ce027ac2b36ec6824a18054e70d899a9f0a57df7
hazemsaad231 <166450789+hazemsaad231@users.noreply.github.com>
2025-02-17 09:21:55 +0200

    uuu

--- src/components/login/image.tsx
+++ src/components/login/image.tsx
@@ -1,24 +1,18 @@
-import { useState, useEffect } from "react";
-import img from "../../assets/img/img.jpeg";
+// import img from "../../assets/img/img.jpeg";
 
 const Image = () => {
-    const [isVisible, setIsVisible] = useState(false);
-    
-    useEffect(() => {
-        const observer = new IntersectionObserver(([entry]) => {
-            if (entry.isIntersecting) setIsVisible(true);
-        });
-        observer.observe(document.getElementById("lazy-image")!);
-        return () => observer.disconnect();
-    }, []);
+   
 
     return (
-        <img
+
+        <div>
+        {/* <img
             id="lazy-image"
-            src={isVisible ? img : ""}
+            src={img}
             alt=""
             className="hidden sm:hidden md:block lg:block xl:block h-screen w-1/2"
-        />
+        /> */}
+        </div>
     );
 };
 
