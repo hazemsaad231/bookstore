3990e25e15dfa709ec49f0e2428cc01365af4301
hazemsaad231 <166450789+hazemsaad231@users.noreply.github.com>
2025-02-17 09:27:44 +0200

    kkkk

--- src/components/login/image.tsx
+++ src/components/login/image.tsx
@@ -1,17 +1,31 @@
-// import img from "../../assets/img/img.jpeg";
+import img from "../../assets/img/img.jpeg";
+import { useState, useEffect } from "react";
 
 const Image = () => {
-   
+    const [showImage, setShowImage] = useState(false);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setShowImage(window.innerWidth >= 768); // تحميل الصورة فقط إذا كانت الشاشة أكبر من 768px
+        };
+
+        handleResize(); // استدعاء أولي للتحقق من الحجم عند التحميل
+        window.addEventListener("resize", handleResize); // تحديث عند تغيير الحجم
+
+        return () => window.removeEventListener("resize", handleResize); // تنظيف الحدث عند تفريغ المكون
+    }, []);
 
     return (
-
         <div>
-        {/* <img
-            id="lazy-image"
-            src={img}
-            alt=""
-            className="hidden sm:hidden md:block lg:block xl:block h-screen w-1/2"
-        /> */}
+            {showImage && (
+                <img
+                    id="lazy-image"
+                    src={img}
+                    alt="Large Screen Image"
+                    loading="lazy"
+                    className="hidden sm:hidden md:block lg:block xl:block h-screen w-1/2"
+                />
+            )}
         </div>
     );
 };
