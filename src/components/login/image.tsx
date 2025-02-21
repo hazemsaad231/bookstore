caebd05e8a03d68777b223f86a2e889604fa2997
hazemsaad231 <166450789+hazemsaad231@users.noreply.github.com>
2025-02-21 16:22:05 +0200

    new

--- src/components/login/image.tsx
+++ src/components/login/image.tsx
@@ -1,14 +1,14 @@
-// import img from "../../assets/img/img.jpeg";
+import img from "../../assets/img/img.jpeg";
 
 const Image = () => {
 
     return (
         <>
-        {/* <img 
+        <img 
           srcSet=  {`${img} 768w`}
          sizes="(min-width: 768px) 50vw, 0px"
           alt="" 
-          className="hidden sm:hidden md:block lg:block xl:block h-screen w-1/2"/> */}
+          className="hidden sm:hidden md:block lg:block xl:block h-screen w-1/2"/>
         
         </>
     )
