4d66348cd6f9fe3acfc6d6f9e40cd2048068a312
hazemsaad231 <166450789+hazemsaad231@users.noreply.github.com>
2025-08-18 16:17:38 +0300

    went

--- src/components/customer/all.tsx
+++ src/components/customer/all.tsx
@@ -8,7 +8,7 @@ const All = () => {
   return(
     <>
     <ToastContainer/>
-    <div className="flex flex-col gap-6 sm:flex-col md:flex-row lg:flex-row xl:flex-row justify-center px-10 py-20 ">
+    <div className="flex flex-col gap-6 sm:flex-col md:flex-row lg:flex-row xl:flex-row justify-center px-2 md:px-6 lg:px-10 py-20 ">
     <Cart/>
     <Payment/>
     </div>
