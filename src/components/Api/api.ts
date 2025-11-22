64bad2db17c7ba1bcb5565a0249e7e273cb1d013
hazemsaad231 <166450789+hazemsaad231@users.noreply.github.com>
2025-11-22 16:44:23 +0200

    next

--- src/components/Api/api.ts
+++ src/components/Api/api.ts
@@ -1,6 +1,7 @@
 
 
- const API_BASE = "https://6793b8915eae7e5c4d8facf8.mockapi.io";
+//  const API_BASE = "https://6793b8915eae7e5c4d8facf8.mockapi.io";
+ const API_BASE = "https://6921cacb512fb4140be15d0b.mockapi.io";
  const API_AUTH = "https://upskilling-egypt.com:3007/api/auth";
 export const BOOKS_API = `${API_BASE}/books`;
 export const Orders_API = `${API_BASE}/orders`;
